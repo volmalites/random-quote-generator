a0cb4717f7c1a5e35634642926d19d8dc0f60aa7
Bjorn Lottering <bjorn@octonet.co.za>
2021-02-11 13:23:32 +0200

    Imported tech degree files
    Basic files including: Object of arrays with quotes, empty main CSS file, basic HTML home page and images for project.

---
+++ js/quotes.js
@@ -0,0 +1,72 @@
+const quotes = [
+  {
+    quote: 'Phall if you but will, rise you must.',
+    source: 'James Joyce',
+    citation: 'Finnegans Wake',
+    year: 1939,
+    photo: 'img/joyce.jpg'
+  },
+  {
+    quote: 'One always has exaggerated ideas about what one doesn\'t know.',
+    source: 'Albert Camus',
+    citation: 'The Stranger',
+    year: 1942,
+    photo: 'img/camus.jpg'
+  },
+  {
+    quote: 'Be kind, for everyone you meet is fighting a hard battle.',
+    source: 'Ian Maclaren',
+    citation: 'The British Weekly',
+    year: 1897,
+    photo: 'img/ian.jpg'
+  },
+  {
+    quote: 'The greatest gift you have is that of your own self-transformation.',
+    source: 'Lao Tzu',
+    citation: 'Tao Te Ching',
+    year: '600BC',
+    photo: 'img/tzu.jpg'
+  },
+  {
+    quote: 'Minds, however, are conquered not by arms, but by love and nobility.',
+    source: 'Baruch Spinoza',
+    citation: 'Ethics',
+    year: 1677,
+    photo: 'img/baruch.jpg'
+  },
+  {
+    quote: 'Courage is the most important of all the virtues because without courage, you can\'t practice any other virtue consistently.',
+    source: 'Maya Angelou',
+    citation: 'Cornell Chronicle',
+    year: 2008,
+    photo: 'img/maya.jpg'
+  },
+  {
+    quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
+    source: 'Martin Luther King',
+    citation: 'Loving Your Enemies',
+    year: 1957,
+    photo: 'img/king.jpg'
+  },
+  {
+    quote: 'People like us, who believe in physics, know that the distinction between past, present and future is only a stubbornly persistent illusion.',
+    source: 'Albert Einstien',
+    citation: 'Letter to Michele Besso',
+    year: 1955,
+    photo: 'img/albert.jpg'
+  },
+  {
+    quote: 'One day, in retrospect, the years of struggle will strike you as the most beautiful.',
+    source: 'Sigmund Freud',
+    citation: 'Letters of Sigmund Freud',
+    year: 1960,
+    photo: 'img/freud.jpg'
+  },
+  {
+    quote: 'Give every man thy ear, but few thy voice.',
+    source: 'Shakespeare',
+    citation: 'Hamlet',
+    year: 1600,
+    photo: 'img/shakespeare.jpg'
+  }
+];
